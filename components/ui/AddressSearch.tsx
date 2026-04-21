0be83ab1515c6771b8c40707916128cd09bdf5d7
Tsukee007 <tsukee@tsukee.fr>
2026-04-21 20:37:19 +0000

    fix: clearTimeout type error

--- components/ui/AddressSearch.tsx
+++ components/ui/AddressSearch.tsx
@@ -66,7 +66,7 @@ export default function AddressSearch({ onSelect }: Props) {
   }
 
   useEffect(() => {
-    return () => clearTimeout(timeoutRef.current)
+    return () => { if (timeoutRef.current) clearTimeout(timeoutRef.current) }
   }, [])
 
   return (
