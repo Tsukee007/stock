7a8082f6f6d586e108a2214fc563c8aebd946b6d
Tsukee007 <tsukee@tsukee.fr>
2026-06-22 09:29:54 +0000

    fix: waitlist env variables types

--- app/api/waitlist/route.ts
+++ app/api/waitlist/route.ts
@@ -2,11 +2,11 @@ import { createClient } from '@supabase/supabase-js'
 import { NextResponse } from 'next/server'
 
 const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.SUPABASE_SERVICE_ROLE_KEY
+  process.env.NEXT_PUBLIC_SUPABASE_URL!,
+  process.env.SUPABASE_SERVICE_ROLE_KEY!
 )
 
-export async function POST(req) {
+export async function POST(req: Request) {
   try {
     const { prenom, email, consent_email, consent_rgpd } = await req.json()
 
